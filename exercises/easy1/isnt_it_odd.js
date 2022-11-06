f7d14b617b2741b1c6cce4c991d8b968cf132d32
Tyler Ramey <tyramey95@gmail.com>
2022-11-06 02:15:17 +0000

    First commit of isnt_it_odd

---
+++ exercises/easy1/isnt_it_odd.js
@@ -0,0 +1,14 @@
+const isOdd = number => {
+  if (number % 2 === 1 || number % 2 === -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isOdd(2)); // => false
+console.log(isOdd(5)); // => true
+console.log(isOdd(-17)); // => true
+console.log(isOdd(-8)); // => false
+console.log(isOdd(0)); // => false
+console.log(isOdd(7)); // => true
