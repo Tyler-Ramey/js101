16dbdf827e9d37a5cb0171845f83c3118caa7280
Tyler Ramey <tyramey95@gmail.com>
2022-11-09 21:08:14 +0000

    Fixed issue causing warning on ESLint

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -3,7 +3,7 @@
 //Ask user for operation
 //Perform the operation
 //Print result
-const LANG = 'cn'
+const LANG = 'cn';
 const MESSAGES = require('./calculator_messages.json');
 const readline = require('readline-sync');
 
@@ -30,59 +30,61 @@ function message(key) {
 let run = true;
 
 while (run) {
-prompt("welcome");
+  prompt("welcome");
 
-prompt("first");
-let number1 = readline.question();
+  prompt("first");
+  let number1 = readline.question();
 
-while (invalidNumber(number1)) {
-  prompt("invalidNumber");
-  number1 = readline.question();
-}
+  while (invalidNumber(number1)) {
+    prompt("invalidNumber");
+    number1 = readline.question();
+  }
 
-prompt("second");
-let number2 = readline.question();
+  prompt("second");
+  let number2 = readline.question();
 
-while (invalidNumber(number2)) {
-  prompt("invalidNumber");
-  number2 = readline.question();
-}
+  while (invalidNumber(number2)) {
+    prompt("invalidNumber");
+    number2 = readline.question();
+  }
 
-prompt("operation");
-let operation = readline.question();
+  prompt("operation");
+  let operation = readline.question();
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("invalidOperation");
-  operation = readline.question();
-}
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt("invalidOperation");
+    operation = readline.question();
+  }
 
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
 
   }
-console.log(`The result is: ${output}`);
+  console.log(`The result is: ${output}`);
 
-prompt("continue");
-let answer = readline.question();
+  prompt("continue");
+  let answer = readline.question();
 
 
-while (invalidContinueResponse(answer)) {
-  prompt("invalidAnswer");
-  answer = readline.question();
-}
+  while (invalidContinueResponse(answer)) {
+    prompt("invalidAnswer");
+    answer = readline.question();
+  }
 
-answer === 'y' ? run = true : run = false;
+  if (answer === 'n') {
+    run = false;
+  }
 
 }
