807e50fdc84d632d975893d97c8ebd4354a0cd9b
Tyler Ramey <tyramey95@gmail.com>
2022-11-13 15:19:08 +0000

    updated loop to accept more options for user input and clear the console on additional calculations

--- lesson_2/loan_calculator/loan_calculator.js
+++ lesson_2/loan_calculator/loan_calculator.js
@@ -1,5 +1,5 @@
 /* Pseudocode */
-/* 
+/*
   Greet user
   Ask user for loan amount, APR, loan duration in years
   Convert loan duration in years to months
@@ -11,7 +11,7 @@
 */
 
 const readline = require('readline-sync');
-const MESSAGES = require('./loan_messages.json')
+const MESSAGES = require('./loan_messages.json');
 
 function prompt(key) {
   console.log(`=> ${MESSAGES[key]}`);
@@ -21,39 +21,53 @@ function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
+function validateUserInput(userChoice) {
+  const VALID_ANSWERS = ['n', 'no', 'y', 'yes'];
+  return VALID_ANSWERS.includes(userChoice);
+}
+
 prompt('welcome');
 
 while (true) {
-  
+
   prompt('amount');
   let loanAmount = readline.question();
-  
+
   while (invalidNumber(loanAmount)) {
     prompt('error');
     loanAmount = readline.question();
   }
-  
+
   prompt('apr');
   let apr = parseFloat(readline.question());
-  
+
   prompt('duration');
   let durationYears = parseFloat(readline.question());
-  
+
   // Conversions
   let durationMonths = durationYears * 12;
   let monthlyIntRate = (apr / 100) / 12; //
-  
+
   let monthlyPayment = Number(loanAmount) * (monthlyIntRate / (1 - Math.pow((1 + monthlyIntRate), (-durationMonths))));
-  
+
   console.log(`Your monthly payment is $${monthlyPayment.toFixed(2)}`);
-  
+
   prompt('continue');
   let userChoice = readline.question();
-  
+  userChoice = userChoice.toLowerCase();
+
+  while (validateUserInput(userChoice) === false) {
+    prompt('invalidAnswer');
+    userChoice = readline.question();
+    userChoice = userChoice.toLowerCase();
+  }
+
   if (userChoice === 'n' || userChoice === 'no') {
     prompt('exit');
     break;
+  } else {
+    console.clear();
   }
-  
+
 }
 
