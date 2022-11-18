979236db8d9242b740033e25e5091d5d922409f0
Tyler Ramey <tyramey95@gmail.com>
2022-11-18 02:21:54 +0000

    Added getUserChoice and getComputerChoice as functions, moved code.

--- lesson_2/rock_paper_scissors/rps.js
+++ lesson_2/rock_paper_scissors/rps.js
@@ -1,5 +1,6 @@
 const readline = require('readline-sync');
 const MESSAGES = require('./rps_messages.json');
+
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 const VALID_ANSWER = [ 'n', 'no', 'y', 'yes'];
 const WINNING_COMBOS = {
@@ -17,6 +18,31 @@ let run = true;
 const prompt = key => {
   console.log(`#>> ${MESSAGES[key]}`);
 };
+
+const displayChoices = () => {
+  console.log(`#>> Choose one: ${VALID_CHOICES.join(', ')}`);
+  prompt('shorthand');
+}
+
+const getUserChoice = () => {
+  displayChoices();
+  let choice = readline.question();
+
+    while (!VALID_CHOICES.includes(choice)) {
+      console.clear();
+      prompt('invalidChoice');
+      displayChoices();
+      choice = readline.question();
+    }
+    
+    return choice;
+}
+
+const getComputerChoice = () => {
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+  return computerChoice;
+}
 
 const playerWins = (choice, computerChoice) => {
   return WINNING_COMBOS[choice].includes(computerChoice);
@@ -39,7 +65,7 @@ const displayWinner = (choice, computerChoice) => {
   } else if (choice === computerChoice) {
     prompt("tie");
   } else {
-    incrementWinner('comuter');
+    incrementWinner('computer');
     prompt("loseRound");
   }
 };
@@ -68,17 +94,15 @@ prompt('welcome');
 while (run) {
 
   while (true) {
-    console.log(playerWinCount, computerWinCount);
-    console.log(`#>> Choose one: ${VALID_CHOICES.join(', ')}`);
-    let choice = readline.question();
-
-    while (!VALID_CHOICES.includes(choice)) {
-      prompt('invalidChoice');
-      choice = readline.question();
+    
+    // Displays score only after the first win;
+    if (playerWinCount !== 0 || computerWinCount !== 0) {
+      console.log(`#>> The sccore is: User - ${playerWinCount} to Computer - ${computerWinCount}`);
     }
-
-    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-    let computerChoice = VALID_CHOICES[randomIndex];
+    
+    let choice = getUserChoice();
+    let computerChoice = getComputerChoice();
+    
 
     displayWinner(choice, computerChoice);
 
