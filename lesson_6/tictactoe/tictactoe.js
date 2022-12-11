3e94584ef859c0de846653a34d06006b1581fd78
Tyler Ramey <tyramey95@gmail.com>
2022-12-11 12:44:06 +0000

    Execution pauses after match, waits for user input before starting next round

--- lesson_6/tictactoe/tictactoe.js
+++ lesson_6/tictactoe/tictactoe.js
@@ -9,7 +9,7 @@ const prompt = msg => {
   console.log(`>>> ${msg}`);
 };
 
-const initializeBoard = () =>{
+const initializeBoard = () => {
   let board = {};
 
   for (let square = 1; square <= 9; square += 1) {
@@ -19,12 +19,12 @@ const initializeBoard = () =>{
   return board;
 };
 
-const displayBoard= (board, playerWins, computerWins) => {
+const displayBoard = (board, playerWins, computerWins) => {
   console.clear();
 
   console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
   console.log(`Player wins: ${playerWins}`);
-  console.log(`Computer wins: ${computerWins}`)
+  console.log(`Computer wins: ${computerWins}`);
 
   console.log('');
   console.log('     |     |');
@@ -85,10 +85,10 @@ const joinOr = (board, delimiter = ',', joinWord = 'or' ) => {
   if (board.length === 0) return '';
   else if (board.length === 1) return `${board[0]}`;
   else if (board.length === 2) return `${board[0]} ${joinWord} ${board[1]}`;
-  
+
   let allButLast = board.slice(0, board.length - 1);
-  let lastNum = board[board.length -1];
-  
+  let lastNum = board[board.length - 1];
+
   return `${allButLast.join(delimiter)} ${joinWord} ${lastNum}`;
 };
 
@@ -129,12 +129,12 @@ const alternatePlayer = currentPlayer => {
 };
 
 while (true) { // Outer Loop
-  let board = initializeBoard();
   let currentPlayer = 'Player';
   let playerWins = 0;
   let computerWins = 0;
 
   while (true) { // Match Loop
+    let board = initializeBoard();
     
     while (true) { // Game Loop
       displayBoard(board, playerWins, computerWins);
@@ -143,30 +143,29 @@ while (true) { // Outer Loop
       if (someoneWon(board) || boardFull(board)) break;
     }
 
-  displayBoard(board, playerWins, computerWins);
+    displayBoard(board, playerWins, computerWins);
 
-  if (someoneWon(board)) {
-    prompt(`${detectWinner(board)} won!`);
-  } else {
-    prompt("It's a tie!");
-  }
-  
-  if (detectWinner(board) === 'Player') playerWins += 1;
-  if (detectWinner(board) === 'Computer') computerWins += 1;
-  
-  if (playerWins === 5 || computerWins === 5) break;
-  
-  setTimeout( () => board = initializeBoard(), 2000)
+    if (someoneWon(board)) {
+      prompt(`${detectWinner(board)} won!`);
+    } else {
+      prompt("It's a tie!");
+    }
     
+    rlsync.keyInPause();
+
+    if (detectWinner(board) === 'Player') playerWins += 1;
+    if (detectWinner(board) === 'Computer') computerWins += 1;
+
+    if (playerWins === WINS_NEEDED || computerWins === WINS_NEEDED) break;
   }
-  
+
   prompt(`Play again? (yes or no)`);
   let answer = rlsync.question().toLowerCase();
   while (!['y', 'yes', 'n', 'no'].includes(answer)) {
     prompt('Please enter yes or no.');
     answer = rlsync.question().toLowerCase();
   }
-  
+
   if (answer === 'no' || answer === 'n') break;
 }
 
