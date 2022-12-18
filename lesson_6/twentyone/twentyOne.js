8ea91ffcc9db21cebbad0c74faaa61da088b3869
Tyler Ramey <tyramey95@gmail.com>
2022-12-18 20:15:42 +0000

    Added best of 5 mode

--- lesson_6/twentyone/twentyOne.js
+++ lesson_6/twentyone/twentyOne.js
@@ -1,9 +1,14 @@
+// Dependencies
 const rlsync = require("readline-sync");
 
+// Global constants
 const SUITS = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
 const VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
 const WINNING_NUMBER = 21;
 const DEALER_STAY_NUMER = 17;
+const ACE_VALUE = 11;
+const COURT_VALUE = 10; // Court cards are Jack, Queen, and King
+const GAMES_TO_WIN = 5;
 
 function prompt (msg) {
   console.log(`>>> ${msg}`);
@@ -85,9 +90,9 @@ function getTotal (cards) {
   let sum = 0;
   values.forEach(value => {
     if (value === "Ace") {
-      sum += 11;
+      sum += ACE_VALUE;
     } else if (['Jack', 'Queen', 'King'].includes(value)) {
-      sum += 10;
+      sum += COURT_VALUE;
     } else {
       sum += Number(value);
     }
@@ -119,20 +124,27 @@ function checkPlayAgain () {
   return false;
 }
 
-function findWinner (pTotal, dTotal) {
+function findWinner (playerTotal, dealerTotal) {
   // Checking for busts
-  if (checkBusted(pTotal)) return 'Dealer';
-  if (checkBusted(dTotal)) return 'Player';
+  if (checkBusted(playerTotal)) return 'Dealer';
+  if (checkBusted(dealerTotal)) return 'Player';
 
   // Comparing scores
-  if (pTotal > dTotal) return 'Player';
-  if (dTotal > pTotal) return 'Dealer';
+  if (playerTotal > dealerTotal) return 'Player';
+  if (dealerTotal > playerTotal) return 'Dealer';
 
   return 'Tie';
 }
 
+function incrementWinner (winner, pWins, dWins) {
+  if (winner === 'Player') pWins += 1;
+  if (winner === 'Dealer') dWins += 1;
+
+  return [ pWins, dWins];
+}
+
 function playerTurn (playerHand, dealerHand, playerTotal, deck) {
-  
+
   while (true) {
     playerTotal = getTotal(playerHand);
     showHands(playerHand, dealerHand, playerTotal);
@@ -148,10 +160,10 @@ function playerTurn (playerHand, dealerHand, playerTotal, deck) {
     let busted = checkBusted(playerTotal);
 
     if (choice === 'stay' || choice === 's') {
-      prompt(`You chose to stay!`);
+      prompt(`You chose to stay!\n`);
       break;
     } else if (busted) {
-      prompt(`You busted! Your total was ${playerTotal}`);
+      prompt(`You busted! Your total was ${playerTotal}\n`);
       break;
     }
   }
@@ -159,54 +171,87 @@ function playerTurn (playerHand, dealerHand, playerTotal, deck) {
   return [ playerHand, playerTotal ];
 }
 
-function dealerTurn (dealerHand, dTotal, deck) {
-  dTotal = getTotal(dealerHand);
-  while (dTotal < DEALER_STAY_NUMER) {
+function dealerTurn (dealerHand, dealerTotal, deck) {
+  
+  while (getTotal(dealerHand) < DEALER_STAY_NUMER) {
     dealerHand.push(deal(deck));
-    dTotal = getTotal(dealerHand);
   }
 
-  return [ dealerHand, dTotal ];
+  return dealerHand;
 }
 
-function game() {
-let pTotal, dTotal;
-
+function roundLoop (playerWins, dealerWins) {
+  let playerTotal;
+  let dealerTotal;
+  
   while (true) {
     let deck = initalizeDeck();
 
     let [ playerHand, dealerHand ] = dealStartingHand(deck);
 
-    [ playerHand, pTotal ] = playerTurn(playerHand, dealerHand, pTotal, deck);
+    [ playerHand, playerTotal ] = playerTurn(playerHand, dealerHand, playerTotal, deck);
 
     // Checks to see if the player busted.
     // If player busted, the dealer is skipped
-    if (!checkBusted(pTotal)) {
-      [ dealerHand, dTotal ] = dealerTurn(dealerHand, dTotal, deck);
+    if (!checkBusted(playerTotal)) {
+      dealerHand = dealerTurn(dealerHand, dealerTotal, deck);
     }
+    
+    dealerTotal = getTotal(dealerHand);
+    
+    prompt(`Dealer has: ${joinHand(dealerHand)}. Total is ${dealerTotal}`);
+    prompt(`Player has: ${joinHand(playerHand)}. Total is ${playerTotal}`);
 
-
-    prompt(`Dealer has: ${joinHand(dealerHand)}. Total is ${dTotal}`);
-    prompt(`Player has: ${joinHand(playerHand)}. Total is ${pTotal}`);
-
-    let winner = findWinner(pTotal, dTotal);
-
+    let winner = findWinner(playerTotal, dealerTotal);
+    
     if (winner === 'Tie') {
-      prompt(`This round ended in a tie!`);
-    } else {
-      prompt(`The ${winner} won this round!`);
+      prompt('That round ended in a tie!\n');
+      return winner;
     }
+    
+    prompt(`The ${winner} won that round!\n`);
+    
+    return winner;
+  }
+}
 
-    if (checkPlayAgain()) break;
-
-    console.clear();
+function game() {
+  
+  let playerWins = 0;
+  let dealerWins = 0;
+  
+  while (true) {
+    while (playerWins < GAMES_TO_WIN && dealerWins < GAMES_TO_WIN) {
+      let winner = roundLoop(playerWins, dealerWins);
+      
+      if (winner === 'Player') playerWins += 1;
+      if (winner === 'Dealer') dealerWins += 1;
+      
+      prompt(`Score\n>>> Player: ${playerWins}\n>>> Dealer: ${dealerWins} `);
+    
+      rlsync.keyInPause('Press any key besides "Enter" to continue.', {guide:false});
+      console.clear();
+    }
+    
+    if (playerWins === GAMES_TO_WIN) prompt(`Great job! You won the game!`);
+    if (dealerWins === GAMES_TO_WIN) prompt(`Bummer.. the dealer won the game.`);
+    
+    let quit = checkPlayAgain();
+    
+    if (quit) break;
+    else {
+      playerWins = 0;
+      dealerWins = 0;
+      console.clear();
+    }
   }
 }
 
 console.clear();
 
 prompt('Welcome to Twenty-One!');
-prompt('Be the first to reach 21 without going over.');
+prompt('Be the first to reach 21 each round without going over.');
+prompt('Win 5 rounds to win the game\n');
 
 game();
 
